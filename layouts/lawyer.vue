fc049a7aa788549d941b08c9e94e4d13f37341d1
郑岩 <zhengyan@jingshonline.com>
2020-01-15 17:36:08 +0800

    Dev

--- layouts/lawyer.vue
+++ layouts/lawyer.vue
@@ -13,7 +13,7 @@ import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 import banner from '@/assets/banner_lawyer.png'
 export default {
-  name: 'IndexLayout',
+  name: 'LawyerLayout',
 
   components: {
     NavBar,
@@ -24,25 +24,16 @@ export default {
       banner: banner,
       url: this.$route
     }
+  },
+  watch: {
+    '$route'(to) {
+      console.log(to)
+      this.url = to
+    }
   }
 }
 </script>
 <style lang="scss">
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
 
 </style>
 
