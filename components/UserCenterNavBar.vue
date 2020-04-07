5e1524cccdb54027f9a448d3a5e77a56a22487e8
郑岩 <zhengyan@jingshonline.com>
2020-04-07 09:04:05 +0800

    userCenterNavBar 调整添加登出

--- components/UserCenterNavBar.vue
+++ components/UserCenterNavBar.vue
@@ -20,16 +20,17 @@
           <el-menu-item index="usercenterexplain"><nuxt-link to="/usercenter/explain">积分说明</nuxt-link></el-menu-item>
 
           <el-menu-item v-if="hasLogin" index="user" class="user">
-            <nuxt-link to="/UserCenter"><el-image :src="account.avatar"/><span>{{ account.name }}</span></nuxt-link>
-            <!-- <el-dropdown class="user_info" placement="bottom-start">
-              <span class="el-dropdown-link">
-                <nuxt-link to="/UserCenter"><el-image :src="account.avatar"/><span>{{ account.name }}</span></nuxt-link>
-              </span>
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item icon="el-icon-user"><nuxt-link to="/userCenter" style="color:#606266;">个人中心</nuxt-link></el-dropdown-item>
-                <el-dropdown-item icon="el-icon-delete" @click.native="handleLogout">登出</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown> -->
+            <nuxt-link to="/usercenter">
+              <el-dropdown class="user_info" trigger="hover">
+                <span class="el-dropdown-link">
+                  <el-image :src="account.avatar"/><span>{{ account.name }}</span>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item icon="el-icon-user"><nuxt-link to="/userCenter/setting" style="color:#606266;">个人中心</nuxt-link></el-dropdown-item>
+                  <el-dropdown-item icon="el-icon-delete" @click.native="handleLogout">登出</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </nuxt-link>
           </el-menu-item>
           <el-menu-item v-else index="login" class="login_btn" @click="loginVisible=true"><span>注册 <b /> 登录</span></el-menu-item>
         </el-menu>
@@ -116,12 +117,12 @@ export default {
   justify-content: center;
   align-items: center;
   .login{
-      float: right;
-      i{
-        margin: 0 10px;
-        border: 1px solid #fff;
-      }
+    float: right;
+    i{
+      margin: 0 10px;
+      border: 1px solid #fff;
     }
+  }
 }
 .el-menu{
   width: 100%;
@@ -136,71 +137,41 @@ export default {
     a{
       padding: 0 30px;
       background: none;
-      display: inline-block;
     }
     &:hover{
       a{
         color: #000;
       }
     }
-    &:first-child:hover{
+    &:first-child:hover,&:last-child:hover{
       background: none;
     }
-    // &:last-child.is-active{
-    //   color: #fff !important;
-    //   background: none;
-    // }
-    // &:last-child{
-    //   float: right;
-    //   span{
-    //     padding: 0 10px;
-    //     &:first-child{
-    //       border-right: 1px solid rgba(255,255,255,.3);
-    //     }
-    //   }
-    //   .el-image{
-    //     width:50px;
-    //     height: 50px;
-    //   }
-    // }
-    // &:last-child:hover{
-    //   a{
-    //     color: #fff;
-    //   }
-    // }
-
-  }
-  .user:hover,.login_btn:hover{
-    background: none;
-  }
-  .login_btn{
-    float: right;
-    &.is-active{
+    &:last-child.is-active{
       color: #fff !important;
       background: none;
     }
-    b{
-      display: inline-block;
-      border-right: 2px solid rgba(255,255,255,.3);
-      height: 16px;
-      margin: 0 20px;
-    }
-  }
-  .user{
-    float: right;
-    span{
-      padding: 0 10px;
-      &:first-child{
-        border-right: 1px solid rgba(255,255,255,.3);
+    &:last-child{
+      float: right;
+      span{
+        padding: 0 10px;
+        &:first-child{
+          border-right: 1px solid rgba(255,255,255,.3);
+        }
+      }
+      .el-image{
+        width:50px;
+        height: 50px;
       }
     }
-    .el-image{
-      width:50px;
-      height: 50px;
+    &:last-child:hover{
+      a{
+        color: #fff;
+      }
     }
     .user_info{
       >span{
         border-right: none !important;
+        color: #fff;
         a{
           padding: 0;
           span{
@@ -212,6 +183,7 @@ export default {
     }
   }
 }
+
 @media (max-width: 1760px) {
   .telephone{
     display: none;
