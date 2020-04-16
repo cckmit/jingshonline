3519378f0b6ea944c27dffe5e51e035720a929fb
郑岩 <zhengyan@jingshonline.com>
2020-04-16 15:09:57 +0800

    添加案件创建编辑所需文件

---
+++ pages/UserCenter/Case/components/StepThree.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    3
+  </div>
+</template>
+<script>
+export default {
+  layout: 'userCenter',
+  name: 'UserCenterCaseStatistical',
+  middleware: 'auth',
+  head() {
+    return {
+      title: '用户中心-案件统计',
+      meta: [
+        { hid: 'description', name: 'description', content: '京师在线用户中心；jingshonline-usercenter' }
+      ]
+    }
+  },
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ pages/UserCenter/Case/create.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right" class="breadcrumb">
+      <el-breadcrumb-item :to="{ path: '/' }">京师在线</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{path:'/userCenter'}">工作台</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{path:'/userCenter/case'}">我的案例</el-breadcrumb-item>
+      <el-breadcrumb-item>添加案例</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'userCenter',
+  name: 'UserCenterCaseStatistical',
+  middleware: 'auth',
+  head() {
+    return {
+      title: '用户中心-案件统计',
+      meta: [
+        { hid: 'description', name: 'description', content: '京师在线用户中心；jingshonline-usercenter' }
+      ]
+    }
+  },
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
