ab103092a62cbbce8d1be485fa598de4c28a23c2
郑岩 <zhengyan@jingshonline.com>
2020-04-16 14:27:14 +0800

    添加案件相关请求方法

--- api/case/index.js
+++ api/case/index.js
@@ -71,3 +71,56 @@ export const caseClick = caseId => {
     method: 'post'
   })
 }
+
+/**
+ * 添加案件
+ * @param {object} caseData
+ */
+export const caseCreate = caseData => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/create`,
+    method: 'post',
+    data: {
+      input: caseData
+    }
+  })
+}
+
+/**
+ * 编辑案件
+ * @param {object} caseData
+ */
+export const caseUpdate = caseData => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/update`,
+    method: 'put',
+    data: {
+      input: caseData
+    }
+  })
+}
+
+/**
+ * 删除案件
+ * @param {number} caseId
+ */
+export const caseDelete = caseId => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/delete/${caseId}`,
+    method: 'delete'
+  })
+}
+
+/**
+ * 查询案件
+ * @param {object} query
+ */
+export const caseQuery = query => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/query`,
+    method: 'post',
+    data: {
+      query
+    }
+  })
+}
