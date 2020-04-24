5f1e00948cea57fec1c0b40d6180e7d0c0497b1c
郑岩 <zhengyan@jingshonline.com>
2020-04-24 10:42:32 +0800

    用户中心相关 case 接口调整

---
+++ api/usercenter/case.js
@@ -0,0 +1,67 @@
+/** 案件 **/
+import request from '@/plugins/request'
+import setting from '@/plugins/setting'
+
+/**
+ * 添加案件
+ * @param {object} caseData
+ */
+export const caseCreate = caseData => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/create`,
+    method: 'post',
+    data: {
+      input: caseData
+    }
+  })
+}
+
+/**
+ * 编辑案件
+ * @param {object} caseData
+ */
+export const caseUpdate = caseData => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/update`,
+    method: 'put',
+    data: {
+      input: caseData
+    }
+  })
+}
+
+/**
+ * 删除案件
+ * @param {number} caseId
+ */
+export const caseDelete = caseId => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/delete/${caseId}`,
+    method: 'delete'
+  })
+}
+
+/**
+ * 查询案件
+ * @param {object} query
+ */
+export const caseQuery = query => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/query`,
+    method: 'post',
+    data: {
+      query
+    }
+  })
+}
+
+/**
+ * 通过案件ID获取案件详情
+ * @param {number} caseId
+ */
+export const caseInfo = caseId => {
+  return request({
+    url: `${setting.apiPrefix}/lawyer/case/${caseId}`,
+    method: 'get'
+  })
+}
